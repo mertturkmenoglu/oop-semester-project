ae42d2994af730af87416f088ddbf70f885fbb0c
mertturkmenoglu <mertturkmenoglu99@gmail.com>
2019-05-09 17:38:16 +0600

    Refactor Date class Add comments, change if-else branch structure, make it easier to read

--- src/ce/yildiz/oop/Date.java
+++ src/ce/yildiz/oop/Date.java
@@ -42,56 +42,94 @@ public class Date {
 		this.year = year;
 	}
 
+	/**
+	 * Check two date object and compare dates.
+	 * If this date is after than other date then return true else false
+	 * @param other is the other date object to compare
+	 * @return date object comparison result
+	 */
 	public boolean isAfterThan(Date other) {
-		if(this.year > other.year) {
+		// Case 1: This date's year is ahead
+		if (this.year > other.year) {
 			return true;
 		} 
 		
-		else if(this.year==other.year) {
-			if(this.month>other.month) {
-				return true;
-			} else if(this.month==other.month) {
-				if(this.day>=other.day) {
-					if(this.isEqualsWith(other)) {
-						return false;
-					}
-					return true;
-				} else  {
-					return false;
-				}
-			} else 
-				return false;
-		} else
+		// Case 2: Other date's year is ahead
+		if (this.year < other.year) {
 			return false;
-	}
-	public boolean isBeforeThan(Date other) {
-		if(this.year<other.year)
-		{
+		}
+		
+		// Years are equal. Check for month and day fields.
+		// Case 3: This date's month is ahead
+		if (this.month > other.month) {
 			return true;
-		} else if(this.year==other.year) {
-			if(this.month<other.month) 
-			{
-				return true;
-			} else if(this.month==other.month) {
-				if(this.day<=other.day) 
-				{
-					if(this.isEqualsWith(other))
-						return false;
-					return true;
-				} else 
-					return false;
-			} else 
-				return false;
-		} else
+		} 
+		
+		// Case 4: Other date's month is ahead
+		if (this.month < other.month) {
+			return false;
+		}
+		
+		// Year and month fields are equal. Check for day field
+		// Case 5: Day fields
+		if (this.day >= other.day) {
+			return !(isEqualsWith(other));
+		} else  {
+			return false;
+		}
+	}
+	
+	/**
+	 * Check two date object and compare dates.
+	 * If this date is before than other date then return true else false
+	 * @param other is the other date object to compare
+	 * @return date object comparison result
+	 */
+	public boolean isBeforeThan(Date other) {
+		// Case 1: This date's year is behind
+		if (this.year < other.year) {
+			return true;
+		} 
+		
+		// Case 2: Other date's year is behind
+		if (this.year > other.year) {
+			return false;
+		}
+		
+		// Year fields are equal. Check for month and day fields.
+		// Case 3: This date's month is behind
+		if (this.month < other.month) {
+			return true;
+		}
+		
+		// Case 4: Other date's month is behind
+		if (this.month > other.month) {
+			return false;
+		}
+		
+		// Year and month fields are equal. Check for day field.
+		// Case 5: Day fields
+		if (this.day <= other.day) {
+			return !(isEqualsWith(other));
+		} else 
 			return false;
 	}
 	
-	public boolean isEqualsWith (Date other) {
-		return ((this.day==other.day) && 
-				(this.month==other.month) && 
-				(this.year==other.year));
+	/**
+	 * Compare two date objects and return a boolean 
+	 * @param other is the other date object to compare
+	 * @return equivalence value
+	 */
+	public boolean isEqualsWith(Date other) {
+		return ((this.day == other.day) && 
+				(this.month == other.month) && 
+				(this.year == other.year));
 	}
 	
+	/**
+	 * Static function to get today's date as date object
+	 * @return a date object holds today's date values
+	 */
 	public static Date getToday() {
 		Calendar cal = new GregorianCalendar();
 		Date temp = new Date();
