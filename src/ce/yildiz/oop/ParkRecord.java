eaf09843097ad89f3c2f8b9b1f06109634d08a08
mertturkmenoglu <mertturkmenoglu99@gmail.com>
2019-05-09 18:25:17 +0600

    Create ParkRecord methods and refactor class

--- src/ce/yildiz/oop/ParkRecord.java
+++ src/ce/yildiz/oop/ParkRecord.java
@@ -1,8 +1,19 @@
 package ce.yildiz.oop;
 
 public class ParkRecord {
-	private Time enterTime,exitTime;
+	private Time enterTime;
+	private Time exitTime;
 	private Vehicle vehicle;
+	
+	public ParkRecord() {
+
+	}
+	
+	public ParkRecord(Time enterTime, Time exitTime, Vehicle vehicle) {
+		this.enterTime = enterTime;
+		this.exitTime = exitTime;
+		this.vehicle = vehicle;
+	}
 	public int getParkingDuration() {
 		return exitTime.getDifference(enterTime);
 	}
