aa9673a80fde512b2317995102259e720f114dcf
mertturkmenoglu <mertturkmenoglu99@gmail.com>
2019-05-09 18:06:19 +0600

    Refactor Subscription class and Add Unit Tests

--- src/ce/yildiz/oop/Subscription.java
+++ src/ce/yildiz/oop/Subscription.java
@@ -1,40 +1,45 @@
 package ce.yildiz.oop;
 
-
 public class Subscription {
-	private Date begin,end;
+	private Date begin;
+	private Date end;
 	private SubscribedVehicle vehicle;
 	
 	public Subscription(Date begin, Date end, String plate) {
 		this.begin = begin;
 		this.end = end;
-		this.vehicle=new SubscribedVehicle(this,plate);
+		this.vehicle = new SubscribedVehicle(this, plate);
 	}
+	
 	public Date getBegin() {
 		return begin;
 	}
+	
 	public void setBegin(Date begin) {
 		this.begin = begin;
 	}
+	
 	public Date getEnd() {
 		return end;
 	}
+	
 	public void setEnd(Date end) {
 		this.end = end;
 	}
+	
 	public SubscribedVehicle getVehicle() {
 		return vehicle;
 	}
+	
 	public void setVehicle(SubscribedVehicle vehicle) {
 		this.vehicle = vehicle;
 	}
+	
 	//Write an Exception !!!
-	boolean isValid() {
-		if((Date.getToday().isAfterThan(begin)&& Date.getToday().isBeforeThan(end) || Date.getToday().isEqualsWith(begin) || Date.getToday().isEqualsWith(end) ))
-			return true;
-		return false;
+	public boolean isValid() {
+		return ((Date.getToday().isAfterThan(begin) && 
+				Date.getToday().isBeforeThan(end) ||
+				Date.getToday().isEqualsWith(begin) || 
+				Date.getToday().isEqualsWith(end) ));
 	}
-	
-	
-
 }
---
+++ src/ce/yildiz/oop/SubscriptionTest.java
@@ -0,0 +1,62 @@
+package ce.yildiz.oop;
+
+import static org.junit.Assert.*;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class SubscriptionTest {
+	private Subscription subscription;
+	private static Date date;
+	private static Date behindDate;
+	private static Date epochDate;
+	private static Date aheadDate;
+	
+	@BeforeClass
+	public static void setUpClass() {
+		date = Date.getToday();
+		behindDate = new Date(1, 1, 1000);
+		epochDate = new Date(1, 1, 1970);
+		aheadDate = new Date(1, 1, 2100);
+	}
+
+	@Test
+	public void testIsValidReturnsTrue() {
+		subscription = new Subscription(epochDate, aheadDate, "34 CE 1000");
+		boolean actual = subscription.isValid();
+		boolean expected = true;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testIsValidReturnsFalse() {
+		subscription = new Subscription(behindDate, epochDate, "34 CE 1000");
+		boolean actual = subscription.isValid();
+		boolean expected = false;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testIsValidTodayDateIsBeginDateReturnsTrue() {
+		subscription = new Subscription(date, aheadDate, "34 CE 1000");
+		boolean actual = subscription.isValid();
+		boolean expected = true;
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	public void testIsValidTodayDateIsEndDateReturnsTrue() {
+		subscription = new Subscription(epochDate, date, "34 CE 1000");
+		boolean actual = subscription.isValid();
+		boolean expected = true;
+		assertEquals(expected, actual);
+	}
+	
+	@AfterClass
+	public static void tearDownClass() {
+		date = null;
+		epochDate = null;
+		aheadDate = null;
+	}
+}
