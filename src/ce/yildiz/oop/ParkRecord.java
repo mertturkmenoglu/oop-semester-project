9e122ab32266838bc521a2b599708ba23d691dd3
mertturkmenoglu <mertturkmenoglu99@gmail.com>
2019-05-12 09:29:42 +0600

    Add getters / setters to ParkRecord

--- src/ce/yildiz/oop/ParkRecord.java
+++ src/ce/yildiz/oop/ParkRecord.java
@@ -14,6 +14,31 @@ public class ParkRecord {
 		this.exitTime = exitTime;
 		this.vehicle = vehicle;
 	}
+	
+	public Time getEnterTime() {
+		return enterTime;
+	}
+
+	public void setEnterTime(Time enterTime) {
+		this.enterTime = enterTime;
+	}
+
+	public Time getExitTime() {
+		return exitTime;
+	}
+
+	public void setExitTime(Time exitTime) {
+		this.exitTime = exitTime;
+	}
+
+	public Vehicle getVehicle() {
+		return vehicle;
+	}
+
+	public void setVehicle(Vehicle vehicle) {
+		this.vehicle = vehicle;
+	}
+
 	public int getParkingDuration() {
 		return exitTime.getDifference(enterTime);
 	}
