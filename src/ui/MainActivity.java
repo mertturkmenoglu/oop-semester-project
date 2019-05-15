9fb7d46b62a4ed4d1d59d8e9becb8ef32176051b
mertturkmenoglu <mertturkmenoglu99@gmail.com>
2019-05-15 22:15:35 +0600

    Update MainActivity class

--- src/ui/MainActivity.java
+++ src/ui/MainActivity.java
@@ -19,12 +19,12 @@ import vehicle.SubscribedVehicle;
 
 @SuppressWarnings("serial")
 public class MainActivity extends JFrame {
-
 	private JPanel contentPane;
 	private AutoPark autoPark;
 
+	
 	/**
-	 * Launch the application.
+	 * This method defines an entry point to program.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -39,6 +39,7 @@ public class MainActivity extends JFrame {
 		});
 	}
 
+	
 	/**
 	 * Create the frame.
 	 */
@@ -56,18 +57,31 @@ public class MainActivity extends JFrame {
 		setSize(screenWidth / 2, screenHeight / 2);
 		setLocation(screenWidth / 4, screenHeight / 4);
 		setTitle("JAVA SEMESTER PROJECT MERT TURKMENOGLU - CAGDAS AYYILDIZ");
+		
 		String hourlyFee;
 		String capacity;
 		
+		/*
+		 * Get hourly fee for autopark object. User should give a valid double value.
+		 * Otherwise, it will ask until user gives.
+		 */
 		do {
 			hourlyFee = (String)
 					JOptionPane.showInputDialog("Otopark saatlik ücreti: ");
 		} while (!Validate.hourlyFee(hourlyFee));
 		
+		
+		/*
+		 * Get autopark capacity. It should be an integer greater than zero.
+		 * On other input cases, it will ask it again.
+		 */
 		do {
 			capacity = (String) JOptionPane.showInputDialog("Kapasite: ");
 		} while (!Validate.capacity(capacity));
 		
+		/*
+		 * Try to create an autopark object.
+		 */
 		try {
 			autoPark = new AutoPark(Double.valueOf(hourlyFee), 
 					Integer.valueOf(capacity));
@@ -76,16 +90,30 @@ public class MainActivity extends JFrame {
 			JOptionPane.showMessageDialog(null, "INVALID VALUES");
 		}
 		
+		/*
+		 * Init Buttons. Buttons will be the medium 
+		 * between user and the program.
+		 */
 		JButton aracEkle = new JButton("Araç Ekle");
 		JButton aracCikar = new JButton("Araç Çýkar");
 		JButton kayitEkle = new JButton("Kayýt Ekle");
 		JButton otoparkGoruntule = new JButton("Otopark Görüntüle");
 		
+		/*
+		 * Add buttons to pane. If you do not add it, 
+		 * it will be initialized but not visible.
+		 */
 		contentPane.add(aracEkle);
 		contentPane.add(aracCikar);
 		contentPane.add(kayitEkle);
 		contentPane.add(otoparkGoruntule);
 		
+		/*
+		 * Add button click listeners to buttons.
+		 * They will listen for button actions.
+		 * When user clicks to button, it will call 
+		 * appropriate methods for each button.
+		 */
 		aracEkle.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
@@ -115,16 +143,33 @@ public class MainActivity extends JFrame {
 		});
 	}
 	
+	
+	/**
+	 * This method handles adding vehicles to autopark.
+	 * It takes vehicle plate, enter time and official vehicle
+	 * information. Then it will try to add it to autopark.
+	 * If a car with the same plate is in the autopark, it should not add it.
+	 * It will display an information dialog.
+	 * If there is no car with the same plate number, it will add it to 
+	 * autopark.
+	 */
 	private void vehicleEnters() {
 		String plate;
 		String enterHour;
 		String enterMinute;
 		
+		/*
+		 * Take vehicle plate
+		 */
 		do {
 			plate = (String)
 					JOptionPane.showInputDialog("Eklenecek aracýn plakasý: ");
 		} while (!Validate.plate(plate));
 
+		/*
+		 * It will display a spinner with two options: "Evet" or "Hayir"
+		 * Answer will be converted to a boolean value.
+		 */
 		Object[] possibilities = {"Evet", "Hayýr"};
 		String isOfficialString = (String)JOptionPane.showInputDialog(
 		                    null, "Resmi araç mý?:\n",
@@ -135,6 +180,9 @@ public class MainActivity extends JFrame {
 		
 		boolean isOfficial = isOfficialString.equals("Evet");
 		
+		/*
+		 * Take time information. First take hour data then minute data.
+		 */
 		do {
 			enterHour = (String)
 					JOptionPane.showInputDialog(
@@ -147,24 +195,44 @@ public class MainActivity extends JFrame {
 							"Aracýn giriþ yaptýðý dakika(sadece dakika bilgisi): ");
 		} while (!Validate.minute(enterMinute));
 		
+		/*
+		 * Try to add it to autopark
+		 */
 		boolean added = autoPark.vehicleEnters(plate, 
 			new Time(Integer.valueOf(enterHour), Integer.valueOf(enterMinute)), 
 			isOfficial);
 		
+		/*
+		 * According to boolean value, print information to screen.
+		 */
 		String message = added ? "Araç Eklendi" : "Araç Eklenemedi";
 		JOptionPane.showMessageDialog(null, message);
 	}
 
+	
+	/**
+	 * This method handles removing vehicles from autopark.
+	 * It takes vehicle plate and exit time informations. 
+	 * Then it will try to remove it from autopark.
+	 * If it can remove it from autopark, it will display
+	 * an information dialog, otherwise, it will prompt an error message.
+	 */
 	private void vehicleExits() {
 		String plate;
 		String exitHour;
 		String exitMinute;
 		
+		/*
+		 * Take vehicle plate
+		 */
 		do {
 			plate = (String)
 					JOptionPane.showInputDialog("Çýkacak aracýn plakasý: ");
 		} while (!Validate.plate(plate));
 		
+		/*
+		 * Take time information. First take hour data then minute data.
+		 */
 		do {
 			exitHour = (String)
 					JOptionPane.showInputDialog(
@@ -177,36 +245,66 @@ public class MainActivity extends JFrame {
 							"Aracýn çýkýþ yaptýðý dakika(sadece dakika bilgisi): ");
 		} while (!Validate.minute(exitMinute));
 		
+		/*
+		 * Try to remove it from autopark
+		 */
 		boolean checkOut = autoPark.vehicleExits(plate,
 				new Time(Integer.valueOf(exitHour), Integer.valueOf(exitMinute)));
 		
+		/*
+		 * According to boolean value, print information to screen.
+		 */
 		String message = checkOut ? "Araç Çýkýþ Yaptý" : "Araç Çýkartýlamadý";
 		JOptionPane.showMessageDialog(null, message);
 	}
 	
+	
+	/**
+	 * This method handles adding subscribed vehicles to autopark system.
+	 * It takes vehicle plate and subscription dates.
+	 * Then it will try to add it to autopark system.
+	 * It will display an information dialog, or, prompt an error message.
+	 */
 	private void addVehicle() {
 		String plate;
 		Date[] dates;
 		Subscription subscription;
 		SubscribedVehicle vehicle;
 		
+		/*
+		 * Take vehicle plate
+		 */
 		do {
 			plate = (String)
 					JOptionPane.showInputDialog("Aracýn plakasý: ");
 		} while (!Validate.plate(plate));
 		
+		/*
+		 * Get subscription's begin and end dates.
+		 * Then create an appropriate subscription with dates and plate.
+		 * Then it will create a vehicle with subscription and plate.
+		 * Then it will try to add it to autopark's subscriptedVehicles array.
+		 */
 		dates = getDate();
 		subscription = new Subscription(dates[0], dates[1], plate);
 		vehicle = new SubscribedVehicle(subscription, plate);
 		
 		boolean added = autoPark.addVehicle(vehicle);
 		
+		/*
+		 * According to boolean value, print information to screen.
+		 */
 		String message = added ? "Araç eklendi" : "Araç eklenemedi";
 		JOptionPane.showMessageDialog(null, message);
 	}
 	
+	
+	/**
+	 * This method handles getting subscription begin and end dates from user.
+	 * It will take fields then try to validate.
+	 * Then it will create Date objects and return them in an array
+	 */
 	private Date[] getDate() {
-		// public Date(int day, int month, int year)
 		Date[] dates = new Date[2];
 		
 		String beginDay;
@@ -218,6 +316,9 @@ public class MainActivity extends JFrame {
 		String beginDate;
 		String endDate;
 		
+		/*
+		 * Take begin and end date informations field by field.
+		 */
 		do {
 			beginDay = (String)
 					JOptionPane.showInputDialog("Baþlangýç günü: ");
@@ -242,6 +343,10 @@ public class MainActivity extends JFrame {
 			endDate = endDay + "-" + endMonth + "-" + endYear;
 		} while (!Validate.date(beginDate) || !Validate.date(endDate));
 		
+		/*
+		 * After validating input, create date objects
+		 * and return them in an array.
+		 */
 		dates[0] = new Date(Integer.valueOf(beginDay), 
 							Integer.valueOf(beginMonth),
 							Integer.valueOf(beginYear));
